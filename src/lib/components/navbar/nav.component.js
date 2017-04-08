6377c569afdc6ff9e73579c694b02d19b56a8ac2
john <jldjld503@gmail.com>
2017-04-07 17:24:58 -0700

    Altered navbar to include logout

--- src/lib/components/navbar/nav.component.js
+++ src/lib/components/navbar/nav.component.js
@@ -2,17 +2,17 @@ import styles from './nav.styles'
 import templateUrl from './nav.template'
 
 const controller = class {
-  static $inject = ['$http']
-  constructor ($http) {
-    this.$http = $http
+  constructor (userstatusservice) {
+    this.userstatusservice = userstatusservice
+  }
+
+  logOut () {
+    this.userstatusservice.logOut()
   }
   styles = styles
 }
 export const navBar = {
   templateUrl,
   controller,
-  bindings: {
-    credentials: '='
-  },
   controllerAs: 'navBar'
 }
